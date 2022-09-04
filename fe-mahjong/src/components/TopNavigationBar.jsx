3378c865b48ad90ca3c28dff2b55dd703b69463a
unknown <daharrisa@gmail.com>
2022-09-04 16:55:51 +0700

    wired to supabase server

--- fe-mahjong/src/components/TopNavigationBar.jsx
+++ fe-mahjong/src/components/TopNavigationBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function TopNavigationBar({ prop_toLeft, prop_toRight }) {
   return (
-    <div className="h-10 flex justify-between bg-[#060629]">
+    <div className="h-10 flex justify-between bg-[#060628]">
       <Link to={prop_toLeft}>
         <div className="p-2">Score submission</div>
       </Link>
