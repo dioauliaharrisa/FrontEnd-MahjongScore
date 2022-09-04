2c07b38a6cf78fa9576076f75e96979498e2a23e
unknown <daharrisa@gmail.com>
2022-09-04 18:16:12 +0700

    testing

--- fe-mahjong/src/components/TopNavigationBar.jsx
+++ fe-mahjong/src/components/TopNavigationBar.jsx
@@ -5,7 +5,7 @@ export default function TopNavigationBar({ prop_toLeft, prop_toRight }) {
   return (
     <div className="h-10 flex justify-between bg-[#060628]">
       <Link to={prop_toLeft}>
-        <div className="p-2">Score submission</div>
+        <div className="p-2 ">Score submission</div>
       </Link>
       <div>{""}</div>
       <Link to={prop_toRight}>
